1afb5d5619846e16e103221e7393f168f52aa474
Kokchun Giang <kokchungiang@gmail.com>
2023-06-01 11:20:33 +0200

    bash operator code alongs

--- Code-along/Lec3_airflow_intro/dags/bash_operator.py
+++ Code-along/Lec3_airflow_intro/dags/bash_operator.py
@@ -9,6 +9,7 @@ data_warehouse_path = Path(__file__).parents[1] /"data"/"data_warehouse"
 
 print(data_lake_path)
 
+time_variable = "$(date +%y%m%d_%H%M%S)"
 
 
 with DAG(dag_id = "joke_DAG", start_date=datetime(2023,6,1)):
@@ -16,7 +17,7 @@ with DAG(dag_id = "joke_DAG", start_date=datetime(2023,6,1)):
 
     setup_folders = BashOperator(task_id = "setup_folders", bash_command=f"mkdir -p {data_lake_path} {data_warehouse_path}")
 
-    download_joke = BashOperator(task_id="random_joke", bash_command=f"curl -o {data_lake_path}/joke.json https://official-joke-api.appspot.com/random_joke" )    
+    download_joke = BashOperator(task_id="random_joke", bash_command=f"curl -o {data_lake_path}/joke_{time_variable}.json https://official-joke-api.appspot.com/random_joke" )    
 
 
     say_hello >> setup_folders >> download_joke
