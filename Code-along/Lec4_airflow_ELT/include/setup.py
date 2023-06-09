32584435e0a697d6008f4ce801c4f7736c776af7
Kokchun Giang <kokchungiang@gmail.com>
2023-06-09 09:28:25 +0200

    ELT modules setup

--- Code-along/Lec4_airflow_ELT/include/setup.py
+++ Code-along/Lec4_airflow_ELT/include/setup.py
@@ -9,6 +9,13 @@ data_warehouse_path = data_path / "data_warehouse"
 
 @task_group(group_id="setup_data_directories")
 def setup_directories():
-    pass
+    create_directories = BashOperator(
+        task_id="create_directories",
+        bash_command=f"mkdir -p {datalake_path} {data_warehouse_path}",
+    )
 
+    success_setup = BashOperator(
+        task_id="setup_success", bash_command="echo 'setup data directories successful'"
+    )
 
+    create_directories >> success_setup
